d5f511dad11d6f66cbb2760eaf7414c0d60f1301
Iain Munro <iain17@me.com>
2016-03-31 14:47:24 +0200

    guice werkt!

--- src/main/java/oose/dea/services/rest/ItemRestService.java
+++ src/main/java/oose/dea/services/rest/ItemRestService.java
@@ -1,53 +1,26 @@
+
 package oose.dea.services.rest;
 
-import javax.inject.Singleton;
-import javax.ws.rs.*;
+import oose.dea.dao.ItemDAO;
+import oose.dea.domain.Item;
+import oose.dea.services.ItemService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
 import java.util.List;
 
 @Path("/items")
-@Singleton
-public class ItemRestService {
-    private List<Item> items = new ArrayList<Item>();
+public class ItemRestService implements ItemService {
 
-    public ItemRestService()
-    {
-        items.add(new Item("Bread", "Breakfast", "Delicious!"));
-        items.add(new Item("Butter", "Breakfast", "Use it with bread"));
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getTextItems()
-    {
-        return "bread, butter";
-    }
+    @Inject
+    private ItemDAO itemDAO;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Item> getJsonItems()  {
-        return items;
-    }
-
-    @GET
-    @Path("/{sku}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Item getJsonItem(@PathParam("sku") final String sku) {
-        return items.stream().filter((item) -> item.getSku().equals(sku)).findAny().get();
-    }
-
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void addItem(Item item)
-    {
-        items.add(item);
-    }
-
-    @DELETE
-    @Path("/{sku}")
-    public void deleteItem(@PathParam("sku") final String sku)
-    {
-        items.removeIf(item -> item.getSku().equals(sku));
+    public List<Item> findAll() {
+        return itemDAO.list();
     }
 }
--- src/main/java/oose/dea/services/rest/RestResourceConfig.java
+++ src/main/java/oose/dea/services/rest/RestResourceConfig.java
@@ -1,7 +1,13 @@
 package oose.dea.services.rest;
 
+import com.google.inject.Guice;
+import oose.dea.guice.AppBinding;
+import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
+import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
+import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/rest")
@@ -9,8 +15,12 @@ class RestResourceConfig extends ResourceConfig {
     public static final String JSON_SERIALIZER = "jersey.config.server.provider.packages";
     public static final String JACKSON_JSON_SERIALIZER = "com.fasterxml.jackson.jaxrs.json;service";
 
-    public RestResourceConfig() {
+    @Inject
+    public RestResourceConfig(ServiceLocator serviceLocator) {
         packages(true, "oose.dea.services.rest");
         property(JSON_SERIALIZER, JACKSON_JSON_SERIALIZER);
+        GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
+        GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+        guiceBridge.bridgeGuiceInjector(Guice.createInjector(new AppBinding()));
     }
 }
