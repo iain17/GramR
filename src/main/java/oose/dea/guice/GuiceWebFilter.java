81b7ee4f001422a6c4ee81acc9b2ab7eb5e781e3
Iain Munro <iain17@me.com>
2016-04-06 16:24:43 +0200

    Don't do a session check for rest calls.

--- src/main/java/oose/dea/guice/GuiceWebFilter.java
+++ src/main/java/oose/dea/guice/GuiceWebFilter.java
@@ -17,20 +17,25 @@ public class GuiceWebFilter extends GuiceFilter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
-//        HttpServletRequest request = (HttpServletRequest) servletRequest;
-//        if(request != null) {
-//            HttpSession session = request.getSession();
-//            if(session != null) {
-//                String owner = (String)session.getValue("owner");
-//
-//                //Do session check:
-//                if(!request.getRequestURI().equals("/index.jsp") && !request.getRequestURI().equals("/") && owner == null) {
-//                    HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-//                    httpResponse.sendRedirect("/");
-//                }
-//
-//            }
-//        }
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        if(request != null) {
+            HttpSession session = request.getSession();
+            if(session != null) {
+                String owner = (String)session.getValue("owner");
+
+                //Don't do this session check for rest calls.
+                if(!request.getRequestURI().matches("/rest")) {
+
+                    //Do session check:
+                    if (!request.getRequestURI().equals("/index.jsp") && !request.getRequestURI().equals("/") && owner == null) {
+                        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+                        httpResponse.sendRedirect("/");
+                    }
+
+                }
+
+            }
+        }
 
         super.doFilter(servletRequest, servletResponse, filterChain);
     }
