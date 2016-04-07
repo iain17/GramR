5f09302ffcf445e9527ac041a34cdda847226156
Iain Munro <iain17@me.com>
2016-04-07 10:20:29 +0200

    use an interface for the test.

--- src/test/java/oose/dea/Photo/PhotoDAOTest.java
+++ src/test/java/oose/dea/Photo/PhotoDAOTest.java
@@ -11,17 +11,17 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class PhotoDAOTest {
-    private PhotoSQLDAO photoSQLDAO;
+    private PhotoDAO photoDAO;
 
     @Before
     public void createClass() {
-        this.photoSQLDAO = new PhotoSQLDAO();
-        this.photoSQLDAO.databaseProperties = new DatabaseProperties();
+        this.photoDAO = new PhotoSQLDAO();
+        ((PhotoSQLDAO)this.photoDAO).databaseProperties = new DatabaseProperties();
     }
 
     @Test
     public void testSuccessInsert() throws Exception {
-        assertEquals(true, photoSQLDAO.insertPhoto(new Photo(500, "Bob", "Mooie foto", "http://google.com/", "geen", null, null)));
+        assertEquals(true, photoDAO.insertPhoto(new Photo(500, "Bob", "Mooie foto", "http://google.com/", "geen", null, null)));
     }
 
     @Test
@@ -29,16 +29,16 @@ public class PhotoDAOTest {
         GrayFilter filter = new GrayFilter();
         filter.setPercentage(75);
 
-        assertEquals(true, photoSQLDAO.insertPhoto(new Photo(500, "Bob", "Mooie foto", "http://google.com/", "geen", null, filter)));
+        assertEquals(true, photoDAO.insertPhoto(new Photo(500, "Bob", "Mooie foto", "http://google.com/", "geen", null, filter)));
     }
 
     @Test
     public void testFailTooLongInsert() throws Exception {
-        assertEquals(false, photoSQLDAO.insertPhoto(new Photo(800, "Bob", "This masterpiece of historical drama presents the voting rights marches in Selma, Alabama in 1965. Though the events happened fifty years ago, this film feels absolutely contemporary. Ava DuVernay directs an enlightening piece at this period in the civil rights movement, drawing out phenomenal acting performances from the talented cast. Test 123", "http://google.com/", "geen", null, null)));
+        assertEquals(false, photoDAO.insertPhoto(new Photo(800, "Bob", "This masterpiece of historical drama presents the voting rights marches in Selma, Alabama in 1965. Though the events happened fifty years ago, this film feels absolutely contemporary. Ava DuVernay directs an enlightening piece at this period in the civil rights movement, drawing out phenomenal acting performances from the talented cast. Test 123", "http://google.com/", "geen", null, null)));
     }
 
     @Test
     public void testFailRequiredFields() throws Exception {
-        assertEquals(false, photoSQLDAO.insertPhoto(new Photo(0, null, null, null, null, null, null)));
+        assertEquals(false, photoDAO.insertPhoto(new Photo(0, null, null, null, null, null, null)));
     }
 }
